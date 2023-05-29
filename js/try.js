cc0259e5d5524d9c22faff23f8d72103dde9f67b
Sofia Titova <titova-sonya.titova@yandex.ru>
2023-05-29 03:30:17 +0300

    kavo

--- js/try.js
+++ js/try.js
@@ -1,3 +1,4 @@
+// НАЧАЛО. Код для работы кнопки на 768px и 320px
 function toggleMenu() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -14,3 +15,46 @@ window.onclick = function(event) {
     }
   }
 }
+// КОНЕЦ. Код для работы кнопки на 768px и 320px
+
+// НАЧАЛО. Код для работы слайдера
+const slider = document.querySelector('.slider');
+const prevArrow = document.querySelector('.prev-arrow');
+const nextArrow = document.querySelector('.next-arrow');
+
+let currentIndex = 0;
+const slides = slider.querySelectorAll('.slide');
+
+function goToSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add('current-slide');
+    } else {
+      slide.classList.remove('current-slide');
+    }
+  });
+}
+
+function slideToPrev() {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = slides.length - 1;
+  }
+  goToSlide(currentIndex);
+}
+
+function slideToNext() {
+  if (currentIndex < slides.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+  goToSlide(currentIndex);
+}
+
+prevArrow.addEventListener('click', slideToPrev);
+nextArrow.addEventListener('click', slideToNext);
+
+goToSlide(currentIndex);
+// КОНЕЦ. Код для работы слайдера
